5f4c60b2731dc94861742ded271c534cf42cc975
YurDan1973 <jura.danilov.73@gmail.com>
2024-12-17 19:35:00 +0300

    Initial commit

--- src/main/java/com/hstn/valid/Client.java
+++ src/main/java/com/hstn/valid/Client.java
@@ -1,11 +1,12 @@
 package com.hstn.valid;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 public class Client {
     private String firstName;
-
 
     @NotNull(message = "Enter last name.")
     // Эта аннотация предупреждает пользователя, что поле не может быть пустым
@@ -14,6 +15,10 @@ public class Client {
     // максимальной длины в символах должнно быть значение поля, а также
     // выводит сообщение если введено неправильно
     private String lastName;
+
+    @Min(value = 18, message = "You must be older then 17.")
+    @Max(value = 125, message = "You must be younger then 126.")
+    private int age;
 
     public String getFirstName() {
         return firstName;
@@ -29,5 +34,13 @@ public class Client {
 
     public void setLastName(String lastName) {
         this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
     }
 }
