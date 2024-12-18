c0308b126d25e40a1336d65f0cac410edfbf4243
YurDan1973 <jura.danilov.73@gmail.com>
2024-12-18 12:40:28 +0300

    Initial commit

--- src/main/java/com/hstn/valid/Client.java
+++ src/main/java/com/hstn/valid/Client.java
@@ -1,9 +1,6 @@
 package com.hstn.valid;
 
-import jakarta.validation.constraints.Max;
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 
 public class Client {
     private String firstName;
@@ -19,6 +16,20 @@ public class Client {
     @Min(value = 18, message = "You must be older then 17.")
     @Max(value = 125, message = "You must be younger then 126.")
     private int age;
+
+    @NotNull
+    @Pattern(regexp = "^[0-9]{5}", message = "Enter valid zipCode. Only numbers, 5 digit.")
+    // В случаях когда требуется чтобы данные вводились
+    // по какому-то шаблону используется эта аннотация
+    // В параметрах этой аннотации указывается регулярные
+    // выражения и сообщение, которое показывается пользователю
+    // в случае если он ввёл неверно
+    // "^[0-9]{5}" - означает, что строка должна начинаться с
+    // цифры от 0 до 9 и таких цифр должно быть 5.
+    // А чтобы пользователь не оставлял поле пустым, кроме того
+    // это поле необходимо пометить аннотацией @NotNull
+
+    private String zipCode;
 
     public String getFirstName() {
         return firstName;
@@ -42,5 +53,13 @@ public class Client {
 
     public void setAge(int age) {
         this.age = age;
+    }
+
+    public String getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(String zipCode) {
+        this.zipCode = zipCode;
     }
 }
